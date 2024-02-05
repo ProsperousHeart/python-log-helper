247c23acc925e71cfa5d74c41e91ea124b625398
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2024-02-05 16:30:07 -0600

    class testing ground

--- Class_Testing/log_helper_as_class.py
+++ Class_Testing/log_helper_as_class.py
@@ -53,6 +53,30 @@ class ConfiguredLogger:
                                             self.logging_loc
                                             )
 
+    def __enter__(self):
+        # self.logger_obj.debug(f"{'='*3} Starting of Logs {'='*3}")
+        self.logger_obj.debug("%s Starting of Logs %s", '='*3, '='*3)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        # self.logger_obj.debug(f"There has been an error of type:\t{exc_type}")
+        self.logger_obj.debug("There has been an error of type:\t%s", exc_type)
+        if exc_val:
+            # self.logger_obj.debug(f"Exception instance::\n{exc_val}")
+            self.logger_obj.debug("Exception instance::\n%s", exc_val)
+        # self.logger_obj.debug(f"TRACEBACK:\n{pprint.pformat(exc_tb)}")
+        self.logger_obj.debug("TRACEBACK:\n%s", pprint.pformat(exc_tb))
+
+        # https://stackoverflow.com/a/7787832/10474024
+        # self.logger_obj.critical(f"""There has been an ERROR!!! Be sure to check your logs:
+        self.logger_obj.critical("There has been an ERROR!!! Be sure to check your logs:\n%s",
+                                 {", ".join(
+            [item.baseFilename for item in self.logger_obj.__dict__['parent'].__dict__['handlers']
+             if item.__class__.__name__ == "FileHandler"])})
+        # self.logger_obj.debug(pprint.pformat(err))
+        # self.logger_obj.debug(f'{"="*3} Ending of Logs {"="*3}')
+        self.logger_obj.debug("%s Ending of Logs %s", '='*3, '='*3)
+
     # ===========================================================================
     # https://docs.python.org/3.12/howto/logging-cookbook.html#how-to-treat-a-logger-like-an-output-stream
     # could be used when considering creation of a class instead of a function
@@ -117,7 +141,7 @@ class ConfiguredLogger:
         """
         return self.logger_obj
 
-
+    @classmethod
     def func_wrapper(self, func):
         """
         Wrapper function to provide start and end logging
@@ -141,49 +165,49 @@ class ConfiguredLogger:
         return log_func_wrapper
 
 
-    def sol_wrapper(self, func):
-        """
-        Wrapper function to provide start and end logging
-        for entire solution - meant to only run ONCE.
-        """
-        @functools.wraps(func)
-        def log_func_wrapper(*args, **kwargs):
-            """
-            Wraps function around DEBUG lines to say start & end
-            of a solution / script. If the script fails,
-            it will log it then gracefully exit so the logs are
-            finalized when closing.
-            """
-            # logger = [arg for arg in args if isinstance(arg, logging.Logger)][0]
-            logger = [
-                arg for arg in args
-                if isinstance(arg, ConfiguredLogger)
-                and isinstance(arg.logger_obj, ConfiguredLoggingObject)
-                # if isinstance(arg, ConfiguredLoggingObject)
-            ][0].logger_obj
-            # ][0]
-            logger.debug(f"{'='*3} Starting of Logs {'='*3}")
-            try:
-                rtn_data = func(*args, **kwargs)
-            except Exception as err:
-                # https://stackoverflow.com/a/7787832/10474024
-                logger.critical(f"""There has been an ERROR!!! Be sure to check your logs:
-                {", ".join([item.baseFilename
-                for item in logger.__dict__['parent'].__dict__['handlers']
-                if item.__class__.__name__ == "FileHandler"])}""")
-                logger.debug(pprint.pformat(err))
-            else:
-                return rtn_data
-            finally:
-                logger.debug(f'{"="*3} Ending of Logs {"="*3}')
-        return log_func_wrapper
+    # def sol_wrapper(self, func):
+    #     """
+    #     Wrapper function to provide start and end logging
+    #     for entire solution - meant to only run ONCE.
+    #     """
+    #     @functools.wraps(func)
+    #     def log_func_wrapper(*args, **kwargs):
+    #         """
+    #         Wraps function around DEBUG lines to say start & end
+    #         of a solution / script. If the script fails,
+    #         it will log it then gracefully exit so the logs are
+    #         finalized when closing.
+    #         """
+    #         # logger = [arg for arg in args if isinstance(arg, logging.Logger)][0]
+    #         logger = [
+    #             arg for arg in args
+    #             if isinstance(arg, ConfiguredLogger)
+    #             and isinstance(arg.logger_obj, ConfiguredLoggingObject)
+    #             # if isinstance(arg, ConfiguredLoggingObject)
+    #         ][0].logger_obj
+    #         # ][0]
+    #         # logger.debug(f"{'='*3} Starting of Logs {'='*3}")
+    #         try:
+    #             rtn_data = func(*args, **kwargs)
+    #         except Exception as err:
+    #             # https://stackoverflow.com/a/7787832/10474024
+    #             logger.critical(f"""There has been an ERROR!!! Be sure to check your logs:
+    #             {", ".join([item.baseFilename
+    #             for item in logger.__dict__['parent'].__dict__['handlers']
+    #             if item.__class__.__name__ == "FileHandler"])}""")
+    #             logger.debug(pprint.pformat(err))
+    #         else:
+    #             return rtn_data
+    #         # finally:
+    #         #     logger.debug(f'{"="*3} Ending of Logs {"="*3}')
+    #     return log_func_wrapper
 
 
 # must have a file_name or else no fileHandler is created
 # even with the default provided in init!
 configured_logger_obj = ConfiguredLogger(file_name="Test_File_As_Class",
                                  file_mode="w")
-sol_wrapper = configured_logger_obj.sol_wrapper
+# sol_wrapper = configured_logger_obj.sol_wrapper
 func_wrapper = configured_logger_obj.func_wrapper
 
 @func_wrapper
@@ -193,7 +217,7 @@ def info_test(log_obj:ConfiguredLoggingObject) -> None:
     """
     log_obj.info("TEST:\tInfo function")
 
-@sol_wrapper
+# @sol_wrapper
 def main(configured_logger:ConfiguredLogger) -> None:
     """
     Takes in a logging object pre-defined for formatting
@@ -207,4 +231,7 @@ def main(configured_logger:ConfiguredLogger) -> None:
     log_obj.warning("This is a warning test ...")
 
 if __name__ == "__main__":
-    main(configured_logger_obj)
+    # main(configured_logger_obj)
+    with ConfiguredLogger(file_name="Test_File_As_Class",
+                          file_mode="w") as conf_logger:
+        main(conf_logger)
--- Class_Testing/main_prog_with_class.py
+++ Class_Testing/main_prog_with_class.py
@@ -4,40 +4,41 @@ import logging
 # from log_helper import sol_wrapper, func_wrapper
 from Class_Testing.log_helper_as_class import ConfiguredLogger
 
-ConfiguredLoggingObject = logging.Logger
+# ConfiguredLoggingObject = logging.Logger
 
-configured_logger_obj = ConfiguredLogger(
-                            file_name="Ext_Test_With_Class",
-                            file_mode="w"
-                        )
-func_wrapper = configured_logger_obj.func_wrapper
-sol_wrapper = configured_logger_obj.sol_wrapper
+# configured_logger_obj = ConfiguredLogger(
+#                             file_name="Ext_Test_With_Class",
+#                             file_mode="w"
+#                         )
+# func_wrapper = configured_logger_obj.func_wrapper
+# sol_wrapper = configured_logger_obj.sol_wrapper
+func_wrapper = ConfiguredLogger.func_wrapper
 
 @func_wrapper
-def debug_test(log_obj:ConfiguredLoggingObject) -> None:
+def debug_test(log_obj:logging.Logger) -> None:
     """
     Function to show usage for DEBUG logging.
     """
     log_obj.debug("TEST:\tDebug function")
 
 @func_wrapper
-def info_test(log_obj:ConfiguredLoggingObject) -> None:
+def info_test(log_obj:logging.Logger) -> None:
     """
     Function to show usage for INFO logging.
     """
     log_obj.info("TEST:\tInfo function")
 
 @func_wrapper
-def warning_test(log_obj:ConfiguredLoggingObject) -> None:
+def warning_test(log_obj:logging.Logger) -> None:
     """
     Function to show usage for WARNING logging.
     """
-    print(log_obj.handlers)
+    print("HANDLERS:\t", log_obj.handlers)
     log_obj.warning("TEST:\tWarning function\n" +
-                    "\tAbout to force a failure ...")
+                    "About to force a failure ...")
 
 @func_wrapper
-def crit_test(log_obj:ConfiguredLoggingObject) -> None:
+def crit_test(log_obj:logging.Logger) -> None:
     """
     Function to show usage for CRITICAL logging.
     """
@@ -49,7 +50,7 @@ Final test - will be a failure!""")
 
     log_obj.info("*** Should never get here ***")
 
-@sol_wrapper
+# @sol_wrapper
 def main(logging_helper_obj:ConfiguredLogger) -> None:
     """
     Main function to kick off the solution
@@ -65,4 +66,7 @@ def main(logging_helper_obj:ConfiguredLogger) -> None:
 
 if __name__ == "__main__":
     # main(create_logger("Ext_Test", "a"))
-    main(configured_logger_obj)
+    # main(configured_logger_obj)
+    with ConfiguredLogger(file_name="Ext_Test_With_Class",
+                          file_mode="w") as conf_logger:
+        main(conf_logger)
