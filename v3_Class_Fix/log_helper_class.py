3c9b643d523080625d9ccbfb510f7210fad5acb1
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2024-02-12 13:50:56 -0600

    tweak to allow for modulation

---
+++ v3_Class_Fix/log_helper_class.py
@@ -0,0 +1,304 @@
+"Module providing easy use of logging."
+# ===========================================================================
+# More information on logging can be found here:
+#   https://www.blog.pythonlibrary.org/2012/08/02/python-101-an-intro-to-logging/
+#   https://www.digitalocean.com/community/tutorials/how-to-use-logging-in-python-3
+#   https://www.loggly.com/ultimate-guide/python-logging-basics/
+# ===========================================================================
+
+import os
+import logging
+import functools
+from datetime import date
+from dataclasses import dataclass
+import traceback
+
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+today = date.today()
+ConfiguredLoggingObject = logging.Logger
+
+
+# ===========================================================================
+# https://docs.python.org/3.12/howto/logging-cookbook.html#how-to-treat-a-logger-like-an-output-stream
+# could be used when considering creation of a class instead of a function
+# ===========================================================================
+@dataclass()
+class ConfiguredLogger:
+    """
+    Class to provide single instance of configured logger & wrappers.
+    """
+    # logging levels:  https://docs.python.org/3/library/logging.html#logging-levels
+    # file_name_in: str = "Test_File_As_Class"
+    file_name_in: str
+    file_mode: str = "a"
+    log_loc: str = f"{os.getcwd()}/logs"
+    datefmt: str = "%Y-%m-%d %H:%M"
+
+    new_exception: int = 0
+    fresh_start: int = 1
+
+    init_file_setup: int = 1
+    file_lvl: int = logging.DEBUG
+    log_file_format: str = logging.Formatter(
+        "%(asctime)s %(levelname)-8s | %(filename)-20s:%(lineno)-5s | %(funcName)-25s %(message)s"
+        )
+
+    init_console_setup: int = 1
+    console_lvl: int = logging.WARNING
+    console_format: str = logging.Formatter("%(asctime)s [%(levelname)-8s] %(message)s\n")
+
+    # logging levels:  https://docs.python.org/3/library/logging.html#logging-levels
+    # https://docs.python.org/3/library/dataclasses.html#dataclasses.__post_init__
+    def __post_init__(self):
+        """
+        Creates & returns a logger object with specific
+        formatting for file and console needs.
+        """
+
+        self.file_name_out: str = f"{self.log_loc}/{today}_{self.file_name_in}.log"
+
+        # if file for writing logs does not exist, create it
+        if not os.path.exists(self.log_loc):
+            os.makedirs(self.log_loc)
+
+        self.logger = logging.getLogger(__name__)    # root logger from main script
+        self.logger.setLevel(logging.DEBUG)
+
+        if self.init_file_setup:
+            self.setup_file_logging()
+
+        if self.init_console_setup:
+            self.setup_console_logging()
+
+        self.logger.info("Logging setup!")
+
+
+    def __enter__(self):
+        return self
+
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type:
+            if self.logger.handlers:
+                self.disable_console_logging()
+            self.logger.warning("Exception occurred...")
+            # self.logger.debug(exc_type)
+            # self.logger.debug(exc_val)
+            # self.logger.debug(pprint.pformat(exc_tb.print_tb, indent=4))
+            # self.logger.debug("Full Traceback:",
+            #                   exc_info=(exc_type, exc_val, exc_tb))
+        self.logger.debug("Ending program ...")
+        self.disable_all_logging()
+
+
+    def setup_console_logging(self):
+        """
+        Setup logging to console.
+        """
+        self.console_handler = logging.StreamHandler()
+        self.console_handler.setLevel(self.console_lvl)
+        self.console_handler.setFormatter(self.console_format)
+        self.enable_console_logging()
+        self.logger.info("Console logging setup")
+
+
+    def setup_file_logging(self):
+        """
+        Setup logging to file.
+        """
+        self.file_handler = logging.FileHandler(self.file_name_out, mode=self.file_mode)
+        self.file_handler.setLevel(self.file_lvl)
+        self.file_handler.setFormatter(self.log_file_format)
+        self.enable_file_logging()
+        self.logger.info("File logging setup")
+
+
+    # def enable_console_logging(self, program_start: int = 1):
+    def enable_console_logging(self):
+        """
+        Enable logging to console.
+        """
+
+        if self.console_handler not in self.logger.handlers:
+            self.logger.addHandler(self.console_handler)
+            self.logger.debug("Console logging enabled")
+
+
+    def enable_file_logging(self):
+        """
+        Setup logging to file.
+        """
+
+        if self.file_handler not in self.logger.handlers:
+            self.logger.addHandler(self.file_handler)
+            self.logger.debug("%s Starting of Logs %s",
+                                '='*3, '='*3)
+            self.logger.debug("File logging enabled")
+
+
+    def disable_console_logging(self):
+        """
+        Disable logging to console.
+        """
+
+        if self.console_handler in self.logger.handlers:
+            self.logger.removeHandler(self.console_handler)
+            if self.logger.handlers:
+                self.logger.debug("Console logging disabled")
+
+
+    def disable_file_logging(self):
+        """
+        Disable logging to file.
+        """
+
+        if self.file_handler in self.logger.handlers:
+            self.logger.removeHandler(self.file_handler)
+            if self.logger.handlers:
+                self.logger.debug("File logging disabled")
+
+
+    def disable_all_logging(self):
+        """
+        Disables all logging - file and console.
+        """
+        if self.file_handler in self.logger.handlers:
+            self.logger.debug("Disabling all logging ...")
+            self.logger.debug("%s Ending of Logs %s",
+                              '='*3, '='*3)
+        for handler in self.logger.handlers[:]:
+            handler.close()
+            self.logger.removeHandler(handler)
+
+
+    def func_wrapper(self, func):
+        """
+        Wrapper function to provide start and end logging
+        when running functions without interfering with
+        other arguments or returned data.
+        """
+        @functools.wraps(func)
+        def log_func_wrapper(*args, **kwargs):
+            # self.logger.debug(f"Starting {func.__qualname__} from module:\t{func.__module__}.{func.__name__}")
+            self.logger.debug("Starting:\t%s.%s", func.__module__, func.__name__)
+            try:
+                rtn_data = func(*args, **kwargs)
+            except Exception as err:
+                if self.new_exception == 0:
+                    self.new_exception = 1
+                    # self.logger.debug(pprint.pformat(err))
+                    self.logger.debug("A %s exception has occurred within %s.%s",
+                                    type(err).__name__,
+                                    func.__module__,
+                                    func.__name__
+                                    )
+                    self.logger.warning("%s:\t%s", type(err).__name__, str(err))
+                    self.disable_console_logging()
+                    self.logger.error("\n%s", traceback.format_exc())
+                    self.enable_console_logging()
+                    self.logger.critical("Log review needed!\nBe sure to check your logs:\n%s",
+                                        next(iter([handler.baseFilename
+                                                for handler in self.logger.handlers
+                                                if isinstance(handler, logging.FileHandler)
+                                                ])
+                                        )
+                    )
+                    # self.logger.info("Exception args:\t%s", err.args)
+                    # self.logger.critical(pprint.pformat(str(err)))
+
+                raise
+            else:
+                return rtn_data
+            finally:
+                # self.logger.debug(f"Ending {func.__qualname__} from module:\t{func.__module__}")
+                self.logger.debug("Ending:\t%s.%s", func.__module__, func.__name__)
+        return log_func_wrapper
+
+
+    def sol_wrapper(self, func):
+        """
+        Wrapper function to provide start and end logging
+        for entire solution - meant to only run ONCE.
+        """
+        @functools.wraps(func)
+        def sol_func_wrapper(*args, **kwargs):
+            """
+            Wraps function around DEBUG lines to say start & end
+            of a solution / script. If the script fails,
+            it will log it then gracefully exit so the logs are
+            finalized when closing.
+            """
+
+            try:
+                rtn_data = func(*args, **kwargs)
+            except Exception as err:
+                self.logger.info("%s exception forced script to close ...", type(err).__name__)
+            else:
+                return rtn_data
+            finally:
+                self.logger.debug("Ending:\t%s.%s", func.__module__, func.__name__)
+
+        return sol_func_wrapper
+
+
+# =========================================
+# Example usage
+# =========================================
+
+logger_obj = ConfiguredLogger(file_name_in="Helper_As_Class_Test_File",
+                              file_mode="w",
+                              init_console_setup=1)
+
+log_obj = logger_obj.logger
+func_wrapper = logger_obj.func_wrapper
+sol_wrapper = logger_obj.sol_wrapper
+
+
+@sol_wrapper
+@func_wrapper
+def main() -> None:
+    """
+    Takes in a logging object pre-defined for formatting
+    then runs a few test functions to confirm use.
+    """
+    # print("Default choices")
+    log_obj.debug("This is a debug test ...")
+    log_obj.info("This is a info test ...")
+    log_obj.warning("This is a warning test ...")
+    # print("initial testing done!")
+
+    assert True is False, "Just testing failure! Does it still finish solution wrap?"
+
+if __name__ == "__main__":
+
+    main()
+
+# if __name__ == "__main__":
+#     with ConfiguredLogger(file_name_in="Helper_As_Class_Test_File",
+#                         file_mode="w",
+#                         init_console_setup=1) as conf_logger:
+
+#         log_obj = conf_logger.logger
+#         func_wrapper = conf_logger.func_wrapper
+#         sol_wrapper = conf_logger.sol_wrapper
+
+#         @sol_wrapper
+#         @func_wrapper
+#         def main() -> None:
+#             """
+#             Takes in a logging object pre-defined for formatting
+#             then runs a few test functions to confirm use.
+#             """
+#             # print("Default choices")
+#             log_obj.debug("This is a debug test ...")
+#             log_obj.info("This is a info test ...")
+#             log_obj.warning("This is a warning test ...")
+#             # print("initial testing done!")
+
+#             assert True is False, "Just testing failure! Does it still finish solution wrap?"
+
+
+
+#         main()
---
+++ v3_Class_Fix/main_prog.py
@@ -0,0 +1,53 @@
+"'Solution' for testing import of logging helper."
+# import logging
+from v2_Class_Testing.log_helper_class import ConfiguredLogger
+
+
+logger_obj = ConfiguredLogger(file_name_in="Test_File_As_Class",
+                              file_mode="w",
+                              init_console_setup=1)
+
+logger = logger_obj.logger
+func_wrapper = logger_obj.func_wrapper
+sol_wrapper = logger_obj.sol_wrapper
+
+@func_wrapper
+def debug_test() -> None:
+    """
+    Function to show usage for DEBUG logging.
+    """
+    logger.debug("TEST:\tDebug function")
+
+@func_wrapper
+def info_test() -> None:
+    """
+    Function to show usage for INFO logging.
+    """
+    logger.info("TEST:\tInfo function")
+    logger.warning("Testing warning message ...")
+
+@func_wrapper
+def crit_test() -> None:
+    """
+    Function to show usage for CRITICAL logging.
+    """
+    logger.info("Final test - a failure!")
+    logger.critical("TEST:\tCritical function - about to fail ...")
+
+    assert True is False, "Just testing failure! Does it still finish solution wrap?"
+
+@sol_wrapper
+@func_wrapper
+def main() -> None:
+    """
+    Main function to kick off the solution
+    when run as a script.
+    """
+    logger.info("Here we gooooooo!!!!!!!!!!")
+    debug_test()
+    info_test()
+    crit_test()
+
+if __name__ == "__main__":
+
+    main()
